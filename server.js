e2a8f6d73ce0dc0335af8911fa9097fa27ad189e
FireAstrox <rtclavell2@alaska.edu>
2024-02-20 19:54:22 -0900

    added support for public directory

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+app.use(express.static('public'));
 app.use(express.json());
 
 const usersFile = 'users.json';
