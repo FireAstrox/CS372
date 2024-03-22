0fe9c4ace020e20e85ef5c37d2a12be74419685a
FireAstrox <rtclavell2@alaska.edu>
2024-03-22 10:10:00 -0800

    Added mongoData File

--- movie_server.js
+++ movie_server.js
@@ -2,4 +2,10 @@ const express = require('express');
 const fs = require('fs').promises;
 const app = express();
 app.use(express.static('Movie Page'));
+app.use(express.json());
 const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended : true }));
+
+const path = require('path');
+
+const mongoData = 'mongoData.json';
