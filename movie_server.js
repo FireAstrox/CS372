1579110781a8abb22027d7f37c544cbd9b53c82e
FireAstrox <rtclavell2@alaska.edu>
2024-03-22 10:23:49 -0800

    :)

--- movie_server.js
+++ movie_server.js
@@ -5,7 +5,12 @@ app.use(express.static('Movie Page'));
 app.use(express.json());
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended : true }));
+const mongoose = require('mongoose');
 
 const path = require('path');
 
 const mongoData = 'mongoData.json';
+
+app.listen(8080, () => {
+    console.log("Server is running on port", 8080);
+});
